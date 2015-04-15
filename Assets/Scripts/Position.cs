8bfad910ad83901e116e9d128a3cf3f2a1bbaf6b
John Caraballo <jcaraballo019@gmail.com>
2015-04-15 01:06:15 -0300

    Variable declarrations

--- Assets/Scripts/Position.cs
+++ Assets/Scripts/Position.cs
@@ -1,8 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Position : MonoBehaviour 
+public class Position : MonoBehaviour
 {
+    private float _randomVertical;
+    private float _randomHorizontal;
+
+
     void OnDrawGizmos()
     {
         Gizmos.DrawWireSphere(transform.position,1f);
